6f65f33ce212bc9e74ae288d79b9d81f610332ae
K. Harrison <kh296@cam.ac.uk>
2022-05-30 16:07:59 +0100

    Make example of local training the default

--- train.sh
+++ train.sh
@@ -27,7 +27,7 @@ black ${CONDA_PREFIX}/lib/python3.7/site-packages/black/cache.py
 #    for cross validation.
 # --monitoring_interval_seconds=120: Check progress every 1200 seconds.
 # --experiment_name: Define name for this training run (experiment).
-python runner.py --azureml=True --model=HeadAndNeckAB442a --num_epoch=30 --train=True --number_of_cross_validation_splits=2 --cluster=NC24 --monitoring_interval_seconds=120 --experiment_name=head_and_neck_ab_120_epochs_5_splits
+#python runner.py --azureml=True --model=HeadAndNeckAB442a --num_epoch=30 --train=True --number_of_cross_validation_splits=2 --cluster=NC24 --monitoring_interval_seconds=120 --experiment_name=head_and_neck_ab_120_epochs_5_splits
 
 # Run training locally.
 # --azureml=False: Don't submit to Azure.
@@ -37,4 +37,4 @@ python runner.py --azureml=True --model=HeadAndNeckAB442a --num_epoch=30 --train
 # --number_of_cross_validation_splits=0: Don't perform cross validation.
 # --monitoring_interval_seconds=50: Check progress every 50 seconds.
 # --experiment_name: Define name for this training run (experiment).
-# python runner.py --azureml=False --model=HeadAndNeckAB_PartIII_001_smg_Local --num_epoch=50 --train=True --number_of_cross_validation_splits=0 --monitoring_interval_seconds=50 --experiment_name=head_and_neck_ab_partiii_001_local_30_epochs_0_splits
+python runner.py --azureml=False --model=HeadAndNeckAB_PartIII_001_smg_Local --num_epoch=50 --train=True --number_of_cross_validation_splits=0 --monitoring_interval_seconds=50 --experiment_name=head_and_neck_ab_partiii_001_local_30_epochs_0_splits
