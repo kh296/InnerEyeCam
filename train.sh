2ad4b09cbd03c3861785689bd38297a947d90d38
K. Harrison <kh296@cam.ac.uk>
2022-06-02 10:17:46 +0100

    Comented second Azure example

--- train.sh
+++ train.sh
@@ -19,7 +19,7 @@ black -q ${CONDA_PREFIX}/lib/python3.7/site-packages/black/cache.py
 # --help : print help information
 # python runner.py --help
 
-# Run training on Azure.
+# Run training on Azure: example 1.
 # --azureml=True: Submit to Azure.
 # --model=HeadAndNeckAB_PartIII_001_smg_Azure: Perform training for model
 #    HeadAndNeckAB_PartIII_001_smg_Azure (.py file in ML/configs/segmentation)
@@ -32,6 +32,17 @@ black -q ${CONDA_PREFIX}/lib/python3.7/site-packages/black/cache.py
 # --experiment_name: Define name for this training run (experiment).
 #python runner.py --azureml=True --model=HeadAndNeckAB_PartIII_001_smg_Azure --num_epoch=30 --train=True --number_of_cross_validation_splits=2 --cluster=NC24 --monitoring_interval_seconds=120 --experiment_name=head_and_neck_ab_partiii_001_azure_30_epochs_2_splits
 
+# Run training on Azure: example 2.
+# --azureml=True: Submit to Azure.
+# --model=HeadAndNeckAB_OSAI_001_smg: Perform training for model
+#    HeadAndNeckAB_OSAI_001_smg (.py file in ML/configs/segmentation)
+# --num_epoch=30: Perform 30 passes of the training data.
+# --number_of_cross_validation_splits=2: Split dataset into 2 parts
+#    for cross validation.
+# --cluster=NC24: Perform training on compute cluster NC24,
+#    in workspace defined in settings.yml.
+# --monitoring_interval_seconds=120: Check progress every 120 seconds.
+# --experiment_name: Define name for this training run (experiment).
 python runner.py --azureml=True --model=HeadAndNeckAB_OSAI_001_smg --num_epoch=30 --train=True --number_of_cross_validation_splits=2 --cluster=NC24 --monitoring_interval_seconds=120 --experiment_name=head_and_neck_ab_osai_001_smg_30_epochs_2_splits
 
 # Run training locally.
